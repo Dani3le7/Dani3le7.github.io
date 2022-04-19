19645ace27da7e27bdb68f4842160e8057b527f6
Daniele Cacciatore <d.cacciatore@teleconsys.it>
2022-04-19 22:48:48 +0200

    Test file php

--- Defenso/Changelog.php
+++ Defenso/Changelog.php
@@ -1,7 +1,2 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
-$changelog ="- Aggiunto riconoscimento biometrico\n- Aggiunto xyz";
-echo $changelog;
-?>
+- Test 123
+- TEst 345657
